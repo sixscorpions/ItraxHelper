ff92916be2b89aabde986cce1bbc8ff9f36e3854
Shankar Pilli <shankar.pilli@sparity.com>
2017-08-04 10:21:26 +0530

    Added login conditions

--- app/src/main/java/com/itraxhelper/MainActivity.java
+++ app/src/main/java/com/itraxhelper/MainActivity.java
@@ -2,10 +2,13 @@ package com.itraxhelper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.LinearLayout;
 
+import com.itraxhelper.models.LoginModel;
+import com.itraxhelper.parser.LoginParser;
 import com.itraxhelper.utils.Constants;
+import com.itraxhelper.utils.Utility;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,20 +16,31 @@ import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.ll_in)
-    LinearLayout ll_in;
-
-    @BindView(R.id.ll_out)
-    LinearLayout ll_out;
+    @BindView(R.id.ll_escort)
+    LinearLayout ll_escort;
 
     @BindView(R.id.ll_mess)
     LinearLayout ll_mess;
+
+    private LoginModel loginModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        LoginParser loginParser = new LoginParser();
+        loginModel = (LoginModel) loginParser.parse(Utility.getSharedPrefStringData(MainActivity.this, Constants.LOGIN_RESPONSE), MainActivity.this);
+        if (loginModel.getHelperAppFeatureModel().getName().equalsIgnoreCase("escort")) {
+            ll_escort.setVisibility(View.VISIBLE);
+            ll_mess.setVisibility(View.GONE);
+        } else if (loginModel.getHelperAppFeatureModel().getName().equalsIgnoreCase("mess")) {
+            ll_escort.setVisibility(View.GONE);
+            ll_mess.setVisibility(View.VISIBLE);
+        } else {
+            ll_escort.setVisibility(View.VISIBLE);
+            ll_mess.setVisibility(View.VISIBLE);
+        }
     }
 
     @OnClick(R.id.ll_mess)
